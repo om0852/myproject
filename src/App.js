c66feeb5034eccb91903c5c1a2fb10bc168ea3b8
om salunke <omsaunke343@gmail.com>
2022-11-05 00:46:28 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import './app1.css';
+import Navbar from './Navbar';
+import { Routes, Route } from "react-router-dom"
+import Textarea from './Textarea';
+import React,{useState} from 'react';
+import About from './About';
+
+function App() {
+  const [mode, setmode] = useState("white");
+  const [mystyle, setmystyle] = useState({
+    color: "black",
+    backgroundColor: "white",
+  });
+  
+  const ontoggle = () => {
+    if (mode === "dark") {
+      setmode("white");
+      setmystyle({
+        color: "black",
+        backgroundColor: "white",
+      });
+    } else {
+      setmode("dark");
+      setmystyle({
+        color: "white",
+        backgroundColor: "blue",
+      });
+    }
+  };
+  return (
+    <div className="App">
+      <Navbar mode={mode} ontoggle={ontoggle} />
+      {/* <Textarea/> */}
+      <Routes>
+        <Route path="/Textarea" element={ <Textarea mystyle={mystyle}/> } />
+        <Route path="/about" element={ <About mystyle={mystyle}/> } />
+        {/* <Route path="contact" element={ <Contact/> } /> */}
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
